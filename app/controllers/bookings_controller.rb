d372cb9ca2e50d39cfc93a80382cc06a46f6fda0
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2024-10-22 18:55:42 +0100

    added mailer view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,15 @@
 class BookingsController < ApplicationController
   def index
   end
+
+  def create
+    TattooMailer.new_request(booking_params).deliver_now
+    redirect_to some_path, notice: 'Tattoo request submitted successfully'
+  end
+
+  private 
+  
+  def booking_params
+    params.require(:booking).permit(:is_adult, :style, :body_part, :size, :description, :artist, tattoo_idea, :legal_consent)
+  end
 end
--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -1,2 +1,9 @@
 class BookingMailer < ApplicationMailer
+    default from: 'luis@codecandy.io'
+
+    def new_request(booking_params)
+        @tattoo_request = booking_params
+        mail(to: "zez608@gmail.com",, subject: "New Tattoo Request")
+    end
+
 end
